087882f3ac57246ea39f1aacd70a0f9bd02a88a7
null <null>
2025-09-09 23:49:33 -0300

    80% feita

--- src/app/pokemon/pokemon.ts
+++ src/app/pokemon/pokemon.ts
@@ -18,7 +18,11 @@ export class Pokemon {
   public altura : number= 0;
   public peso : number= 0;
   public hp : number=0;
-  public tipo: string = '';
+  public tipo1: string = '';
+  public tipo2: string = '';
+  public ataque: number=0;
+  public defesa: number=0;
+  public agilidade: number=0;
 
   constructor(private http:HttpClient){
       this.pesquisar()
@@ -34,7 +38,12 @@ export class Pokemon {
         this.altura = dados.height;
         this.peso = dados.weight;
         this.hp = dados.stats[0].base_stat;
-        this.tipo = dados.types[0].type.name;
+        this.tipo1 = dados.types[0].type.name;
+        this.tipo2 = dados.types[1].type.name;
+        this.ataque = dados.stats[1].base_stat;
+        this.defesa = dados.stats[2].base_stat;
+        this.agilidade = dados.stats[5].base_stat;
+
 
         console.log(dados);
       });
